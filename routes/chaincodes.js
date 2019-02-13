44fe90da18abd100195f23734f5fa718c26732fc
NRajTheGeek <rajputneerajkumar815@gmail.com>
2019-02-13 15:20:51 +0530

    upgrade chaincode added

--- routes/chaincodes.js
+++ routes/chaincodes.js
@@ -7,6 +7,8 @@ var helper = require("../utils/helper");
 
 var install = require("../utils/install-chaincode.js");
 var instantiate = require("../utils/instantiate-chaincode.js");
+var upgrade = require("../utils/upgrade-chaincode");
+
 var invoke = require("../utils/invoke-transaction.js");
 
 // Install chaincode on target peers
@@ -107,6 +109,61 @@ router.post("/channels/:channelName/chaincodes", function(req, res) {
       res.send(message);
     });
 });
+
+// upgrade chaincode on target peers
+router.put("/channels/:channelName/chaincodes", function(req, res) {
+  console.log("==================== upgrade CHAINCODE ==================");
+  var chaincodeName = req.body.chaincodeName;
+  var chaincodeVersion = req.body.chaincodeVersion;
+  var channelName = req.params.channelName;
+  var fcn = req.body.fcn;
+  var args = req.body.args;
+  var targets = req.body.peers;
+  var userName = req.body.userName;
+  var orgName = req.body.orgName;
+
+  if(!fcn) {
+    fcn = "";
+  }
+
+  console.log("channelName  : " + channelName);
+  console.log("chaincodeName : " + chaincodeName);
+  console.log("chaincodeVersion  : " + chaincodeVersion);
+  console.log("fcn  : " + fcn);
+  console.log("args  : " + args);
+  if (!chaincodeName) {
+    res.json(helper.getErrorMessage("'chaincodeName'"));
+    return;
+  }
+  if (!chaincodeVersion) {
+    res.json(helper.getErrorMessage("'chaincodeVersion'"));
+    return;
+  }
+  if (!channelName) {
+    res.json(helper.getErrorMessage("'channelName'"));
+    return;
+  }
+  if (!args) {
+    res.json(helper.getErrorMessage("'args'"));
+    return;
+  }
+  upgrade
+    .upgradeChaincode(
+      targets,
+      channelName,
+      chaincodeName,
+      chaincodeVersion,
+      fcn,
+      "golang",
+      args,
+      userName,
+      orgName
+    )
+    .then(function(message) {
+      res.send(message);
+    });
+});
+
 // Invoke transaction on chaincode on target peers
 router.post("/channels/:channelName/chaincodes/:chaincodeName", function(
   req,
